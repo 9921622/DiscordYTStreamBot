142baeb86bb26ef9e0dfad5e612bdcbba4c304ed
9921622 <marc.alfre.go@gmail.com>
2026-03-26 13:21:19 -0700

    Chore: testcase foundation

---
+++ apps/discordbot/src/tests/test_admin.py
@@ -0,0 +1,19 @@
+# def test_leave(client):
+#     url = client.app.url_for("admin-leave")
+#     response = client.get(url)
+#     assert response.status_code == 200
+
+# def test_chat(client):
+#     url = client.app.url_for("admin-chat")
+#     response = client.get(url)
+#     assert response.status_code == 200
+
+# def test_guilds(client):
+#     url = client.app.url_for("admin-guilds")
+#     response = client.get(url)
+#     assert response.status_code == 200
+
+# def test_channels(client):
+#     url = client.app.url_for("admin-channels")
+#     response = client.get(url)
+#     assert response.status_code == 200

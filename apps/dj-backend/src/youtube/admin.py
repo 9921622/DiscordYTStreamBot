575f836810e899e9a71b8c500d0caf2a6babddcf
9921622 <marc.alfre.go@gmail.com>
2026-03-25 19:49:58 -0700

    Chore: admin forms for youtube models

--- apps/dj-backend/src/youtube/admin.py
+++ apps/dj-backend/src/youtube/admin.py
@@ -1,3 +1,37 @@
 from django.contrib import admin
 
-# Register your models here.
+from youtube.models import YoutubeTag, YoutubeVideo, YoutubePlaylist, YoutubePlaylistItem
+
+
+@admin.register(YoutubeTag)
+class YoutubeTagAdmin(admin.ModelAdmin):
+    list_display = ["name"]
+    search_fields = ["name"]
+    ordering = ["name"]
+
+
+@admin.register(YoutubeVideo)
+class YoutubeVideoAdmin(admin.ModelAdmin):
+    list_display = ["youtube_id", "title", "creator", "duration", "created_at"]
+    list_filter = ["created_at", "creator"]
+    search_fields = ["youtube_id", "title", "creator"]
+    readonly_fields = ["youtube_id", "created_at"]
+    filter_horizontal = ["tags"]
+    ordering = ["-created_at"]
+
+
+@admin.register(YoutubePlaylist)
+class YoutubePlaylistAdmin(admin.ModelAdmin):
+    list_display = ["name", "playlist_type", "user", "created_at"]
+    list_filter = ["playlist_type", "created_at"]
+    search_fields = ["name", "user__username"]
+    readonly_fields = ["created_at", "updated_at"]
+    ordering = ["-created_at"]
+
+
+@admin.register(YoutubePlaylistItem)
+class YoutubePlaylistItemAdmin(admin.ModelAdmin):
+    list_display = ["playlist", "video", "order"]
+    list_filter = ["playlist", "order"]
+    search_fields = ["playlist__name", "video__title"]
+    ordering = ["playlist", "order"]

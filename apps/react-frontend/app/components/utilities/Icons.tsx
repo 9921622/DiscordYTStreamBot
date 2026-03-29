6c733fc449dbff976983787ec9ee7db5f57a6082
9921622 <marc.alfre.go@gmail.com>
2026-03-28 18:59:31 -0700

    Chore: icon reference link

--- apps/react-frontend/app/components/utilities/Icons.tsx
+++ apps/react-frontend/app/components/utilities/Icons.tsx
@@ -1,3 +1,8 @@
+
+/*
+    ALL ICONS ARE GRABBED FROM https://www.svgrepo.com/
+*/
+
 function CreateIcon(svg: string) {
     return (
         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2" className="size-5"> <path strokeLinecap="round" strokeLinejoin="round" d={svg}/> </svg>

363347e09e73d67c9588a4ff97bde6f6fdb141b7
9921622 <marc.alfre.go@gmail.com>
2026-03-30 01:13:57 -0700

    Feat: token refresh view

--- apps/dj-backend/src/backend/urls.py
+++ apps/dj-backend/src/backend/urls.py
@@ -17,10 +17,12 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework_simplejwt.views import TokenRefreshView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api-auth/", include("rest_framework.urls")),
+    path("api/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
     path("api/youtube/", include("youtube.urls", namespace="youtube")),
     path("api/discord/", include("discord.urls", namespace="discord")),
 ]

fc219f23b1da0ea47c8f6452ccdd3152b8b22144
9921622 <marc.alfre.go@gmail.com>
2026-03-30 01:29:17 -0700

    Chore: working logout

--- apps/react-frontend/app/components/Navbar.tsx
+++ apps/react-frontend/app/components/Navbar.tsx
@@ -9,6 +9,12 @@ import { useNavigate } from "react-router";
 function ProfileDropdown({ profile } : { profile? : DiscordUser }) {
   const navigate = useNavigate()
 
+  const handleLogout = () => {
+    localStorage.removeItem('access')
+    localStorage.removeItem('refresh')
+    navigate('/login')
+  }
+
   if (!profile) return (
     <button className="btn btn-sm btn-primary" onClick={() => navigate('/login')}>
       Login
@@ -17,40 +23,18 @@ function ProfileDropdown({ profile } : { profile? : DiscordUser }) {
 
   return (
     <div className="dropdown dropdown-end">
-          <div
-            tabIndex={0}
-            role="button"
-            className="btn btn-ghost btn-circle avatar"
-          >
-            <div className="w-10 rounded-full">
-              {
-                profile &&
-                  (<img
-                  alt="Profile"
-                  src={profile.avatar}
-                  />)
-              }
-            </div>
-          </div>
-          <ul
-            tabIndex={-1}
-            className="menu menu-sm dropdown-content bg-base-100 rounded-box z-10 mt-3 w-52 p-2 shadow"
-          >
-            <li>
-              <a className="justify-between">
-                Profile
-                <span className="badge">New</span>
-              </a>
-            </li>
-            <li>
-              <a>Settings</a>
-            </li>
-            <li>
-              <a>Logout</a>
-            </li>
-          </ul>
+      <div tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar">
+        <div className="w-10 rounded-full">
+          {profile && <img alt="Profile" src={profile.avatar} />}
         </div>
-  );
+      </div>
+      <ul tabIndex={-1} className="menu menu-sm dropdown-content bg-base-100 rounded-box z-10 mt-3 w-52 p-2 shadow">
+        <li><a className="justify-between">Profile<span className="badge">New</span></a></li>
+        <li><a>Settings</a></li>
+        <li><a onClick={handleLogout}>Logout</a></li>
+      </ul>
+    </div>
+  )
 }
 
 

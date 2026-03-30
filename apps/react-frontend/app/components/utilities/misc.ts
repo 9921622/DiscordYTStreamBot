59dd4d33712944e0ae7d2eab07f6380c88119010
9921622 <marc.alfre.go@gmail.com>
2026-03-30 10:12:28 -0700

    Tweak: NumToTime account for hours

--- apps/react-frontend/app/components/utilities/misc.ts
+++ apps/react-frontend/app/components/utilities/misc.ts
@@ -1,7 +1,13 @@
 
 
 export function NumToTime(num: number): string {
-    const minutes = Math.floor(num / 60);
+    const hours = Math.floor(num / 3600);
+    const minutes = Math.floor((num % 3600) / 60);
     const seconds = Math.floor(num % 60);
+
+    if (hours > 0) {
+        return `${hours}:${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`;
+    }
+
     return `${minutes}:${seconds.toString().padStart(2, '0')}`;
 }

c91f80f9e797d0bcc970f2ae3dcd45d0bcd65d5b
9921622 <marc.alfre.go@gmail.com>
2026-04-02 05:18:51 -0700

    Fix: user command stopping vc

--- apps/discordbot/src/api/websockets/commands/voice.py
+++ apps/discordbot/src/api/websockets/commands/voice.py
@@ -6,5 +6,4 @@ class UsersCommand(WebsocketCommand):
     prefix = "users"
 
     async def handle(self):
-        await bot.vc_stop(self.guild_id)
         return self.response(members=bot.vc_get_members(self.guild_id).model_dump())

397fa4536ba8666d94ee796605a4687ce99707d1
9921622 <marc.alfre.go@gmail.com>
2026-04-04 07:44:26 -0700

    Fix: add authorization token to all searches

--- apps/react-frontend/app/middleware.ts
+++ apps/react-frontend/app/middleware.ts
@@ -25,3 +25,11 @@ axios.interceptors.response.use(
         return Promise.reject(error)
     }
 )
+
+axios.interceptors.request.use(config => {
+    const token = localStorage.getItem('access')
+    if (token) {
+        config.headers['Authorization'] = `Bearer ${token}`
+    }
+    return config
+})
